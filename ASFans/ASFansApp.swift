fc5e60e7aab31443a233787f6eae555b66026503
yanlc <xush2014@hotmail.com>
2022-04-07 02:25:21 +0800

    Initial Commit

---
+++ ASFans/ASFansApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASFansApp.swift
+//  ASFans
+//
+//  Created by 言临川 on 2022/4/7.
+//
+
+import SwiftUI
+
+@main
+struct ASFansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASFans/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ASFans
+//
+//  Created by 言临川 on 2022/4/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
